ddfda33cdd056aad07b4a3ded878f7ec9f3f1f58
Ryan Downing <26515643+r-downing@users.noreply.github.com>
2020-04-12 00:02:23 -0400

    separate out some js utils

---
+++ utils.js
@@ -0,0 +1,13 @@
+function hashCode(s) {
+    return s.split("").reduce(function (a, b) { a = ((a << 5) - a) + b.charCodeAt(0); return a & a }, 0);
+}
+
+function xoshiro128ss(a, b, c, d) {
+    return function () {
+        var t = b << 9, r = a * 5; r = (r << 7 | r >>> 25) * 9;
+        c ^= a; d ^= b;
+        b ^= c; a ^= d; c ^= t;
+        d = d << 11 | d >>> 21;
+        return (r >>> 0) / 4294967296;
+    }
+}
